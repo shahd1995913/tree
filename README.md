be81d1a7869aa21ebedda445a9a4107fdafe4b6c
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-03 06:36:36 +0400

    first day -2021-12-3

--- README.md
+++ README.md
@@ -12,8 +12,7 @@ Search complexity is O(logn)
 n=8 8->4->2->1
 3 itreations  log2=3
 
- 1. [Binary search tree](https://www.youtube.com/watch?v=lFq5mYUWEBk) 
-
+- <a href="https://www.youtube.com/watch?v=lFq5mYUWEBk">Binary search tree -- link</a>
 
 <h1 style="color: Pink; opacity: 0.80;"> Linear Search  </strong>
 --> In linear Search if the n=8 then 
@@ -26,7 +25,9 @@ mean 8 itreations <h1 style="color: orange; opacity: 0.80;"> Breadth First Searc
 1. In order traversal
 2. Pre order traversal
 3. post order traversal
-- [Delete Node in tree vidue](https://www.youtube.com/watch?v=JnrbMQyGLiU)
-- [Delete task](https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/9_Binary_Tree_2/9_binary_tree_part_2_exercise.md)
-- [tree min , max , post ,pre order in Binary tree search  task](https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/8_Binary_Tree_1/8_binary_tree_part_1_exercise.md)
+
+- <a href="https://www.youtube.com/watch?v=JnrbMQyGLiU">Delete Node in tree vidue-- link</a>
+- <a href="https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/8_Binary_Tree_1/8_binary_tree_part_1_exercise.md">tree min , max , post ,pre order in Binary tree search  task--link</a>
+- <a href="https://github.com/codebasics/data-structures-algorithms-python/blob/master/data_structures/9_Binary_Tree_2/9_binary_tree_part_2_exercise.md">Delete task--link</a>
+
 ![tree](tree.png)
--- bts.py
+++ bts.py
@@ -85,7 +85,26 @@ class BinaryTreeNode:
            self.right=self.right.delete(min_val)
 
         return self
-            
+
+    def sum_tree(self):
+        left_sum=self.left.sum_tree() if self.left else 0
+        right_sum =self.right.sum_tree() if self.right else 0
+        return self.data +left_sum+ right_sum
+    def post_order(self):
+        element=[]
+        if self.left:
+            element+=self.left.post_order()
+        if self.right:
+            element+=self.right.post_order()
+        element.append(self.data)
+        return element
+    def pre_order(self):
+        element=[self.data]
+        if self.left:
+            element+=self.left.pre_order()
+        if self.right:
+            element+=self.right.pre_order()
+        return element
 
 
 def bulid_tree(element):
@@ -100,14 +119,16 @@ def bulid_tree(element):
 if __name__=="__main__":
     num=[17,4,0,200,9,23,18,34]
     num_tree =bulid_tree(num)
-    print(num_tree.in_order())
-    print(num_tree.serach(17))
-    print(num_tree.serach(100))
-    print(num_tree.find_max())
-    print(num_tree.find_min())
-    num_tree.delete(18)
-    print(num_tree.in_order())
-
+    print("In order in tree ",num_tree.in_order())
+    print("Search about 17 in tree is ",num_tree.serach(17))
+    print("Search about 100 in tree is ",num_tree.serach(100))
+    print("The max value in tree",num_tree.find_max())
+    print("The min value in tree",num_tree.find_min())
+    num_tree.delete(23)
+    print("After delete a value from tree",num_tree.in_order())
+    print("sum all node value in tree ",num_tree.sum_tree())
+    print("Post order tree",num_tree.post_order())
+    print("Pre order tree",num_tree.pre_order())
 
 
 
